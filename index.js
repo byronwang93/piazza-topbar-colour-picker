308aa76d692281a5bd5c1679dea87b67eb0e2148
UBC Student <bwang00@pender.students.cs.ubc.ca>
2022-01-15 15:18:47 -0800

    updated html json and js files + added logo image

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var topbar = document.getElementById("topbar");
 var inputText = document.getElementById("new-colour");
+var userColour;
+const colorButton = document.querySelector(".color-button");
+
+// topbar.stylebackgroundColor = "green";
+
+document.getElementById("colour-button").addEventListener("click", changeColour);
+
+function changeColour() {
+    userColour = inputText.value;
+    topbar.style.backgroundColor = userColour;
+}
