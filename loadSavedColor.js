0eb516ade02ecae285ee270759b35bb7a927249b
AlexLuo602 <60013046+AlexLuo602@users.noreply.github.com>
2022-01-15 23:37:59 -0800

    made it so extension doesn't run on non piazza sitees

--- loadSavedColor.js
+++ loadSavedColor.js
@@ -1,15 +1,20 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.storage.sync.get("savedColor", function(result) {
-      console.log('Value currently is ' + result.savedColor);
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    let url = tabs[0].url;
+    if (url.includes("piazza")) {
+      chrome.storage.sync.get("savedColor", function(result) {
+        console.log('Value currently is ' + result.savedColor);
 
-      if (result.savedColor) {
-        var userInput = result.savedColor
+        if (result.savedColor) {
+          var userInput = result.savedColor
 
-        chrome.tabs.executeScript({code:"var userInput = '"+userInput+"'; try{topbar.style.backgroundColor = userInput;}catch(e){alert(e);}"},null);
-      } else {
-        var userInput = "green"
-        chrome.tabs.executeScript({code:"var userInput = '"+userInput+"'; try{topbar.style.backgroundColor = userInput;}catch(e){alert(e);}"},null);
-      }
-     
-      });
+          chrome.tabs.executeScript({code:"var userInput = '"+userInput+"'; try{topbar.style.backgroundColor = userInput;}catch(e){alert(e);}"},null);
+        } else {
+          var userInput = "green"
+          chrome.tabs.executeScript({code:"var userInput = '"+userInput+"'; try{topbar.style.backgroundColor = userInput;}catch(e){alert(e);}"},null);
+        }
+      
+        });
+    }
+    });
 });
