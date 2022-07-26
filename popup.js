1b8538c6f5a7bf124a3cc4b384946c48df000525
Byron Wang <byronwang123919@gmail.com>
2022-07-25 23:45:43 -0700

    fix piazza bug

--- popup.js
+++ popup.js
@@ -1,17 +1,25 @@
-
-
 if (document.querySelector(".popup")) {
+  if (document.getElementById("topbar")) {
     const topbar = document.getElementById("topbar");
 
     const colourButton = document.querySelector(".colour-button");
 
     colourButton.addEventListener("click", () => {
-        var userInput = document.getElementById("new-colour").value;
+      var userInput = document.getElementById("new-colour").value;
 
-        chrome.storage.sync.set({"savedColor": userInput}, function() {
-            console.log('Value is set to ' + userInput);
-          });
+      chrome.storage.sync.set({ savedColor: userInput }, function () {
+        console.log("Value is set to " + userInput);
+      });
 
-        chrome.tabs.executeScript({code:"var userInput = '"+userInput+"'; try{topbar.style.backgroundColor = userInput;}catch(e){alert(e);}"},null);
+      chrome.tabs.executeScript(
+        {
+          code:
+            "var userInput = '" +
+            userInput +
+            "'; try{topbar.style.backgroundColor = userInput;}catch(e){alert(e);}",
+        },
+        null
+      );
     });
+  }
 }
